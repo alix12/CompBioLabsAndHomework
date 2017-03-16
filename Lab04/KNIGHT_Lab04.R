de916c5955b682001e71c3ec5f3c8e02a0a2ca1c
alix12 <alkn5295@colorado.edu>
2017-03-15 22:12:09 -0600

    updated, took out more magic numbers, fixed some mistakes, question 4 still in progress

--- Lab04/KNIGHT_Lab04.R
+++ Lab04/KNIGHT_Lab04.R
@@ -16,12 +16,16 @@ for( i in seq(1,10) ){
 ### How much money will tim have in 8 weeks? 
 ### Write a loop that prints to the console
 ### equation: Y = 10 + 5i -2.68i
+## start value
+init_piggy <- 10
+allowance <- 5
+gum <- 1.34
 
 Weeks <- rep(0,8)
 for( i in seq(1,8) ) {
-  Weeks <- 10 + (5 * i) - (2.68 * i)
-  print(Weeks)
+  Weeks[i] <- init_piggy + (allowance * i) - ((2*gum) * i)
 }
+Weeks[8]
 
 ### Answer: Tim will have $28.56 in 8 weeks
 
@@ -29,23 +33,26 @@ for( i in seq(1,8) ) {
 ### Starting with a population of 2000 individuals
 ### The population shrinks by 5% each year
 ### What is the population size each year for the next seven years?
-### Equation: 2000 - ((2000 * 0.05) * t)  
+### Equation: (2000 * .95 * t)  
 init_pop <- 2000
-Population <- rep(0,7)
-for( i in seq(0,7)) {
-  Population <- 2000 - ((init_pop * 0.05) * i)
+decline <- 0.95
+years <- 7
+
+for( i in 1:years ) {
+  Population <- (init_pop * decline)
   print(Population)
+  init_pop <- Population
 }
 
-### ANSWER
-###0 2000
-###1 1900
-###2 1800
-###3 1700
-###4 1600
-###5 1500
-###6 1400
-###7 1300
+### ANSWERs
+### 1900
+### 1805
+### 1714.75
+### 1629.012
+### 1547.562
+### 1470.184
+### 1396.675 
+
 
 ### Question FOUR
 ### discrete-time logistic qrowth equation:
@@ -99,24 +106,85 @@ numbers
 ### make a vector of the first 20 numbers in the fibonacci seq by using a for loop
 ### equation: value = i-1 + i-2
 
-fib_seq <- rep(1,20)
+length_of_fib <- 20
+### compatible with whatever lenght of fibonacci sequence for the rest of the code
+fib_seq <- rep(1,length_of_fib)
 
-for( i in 2:20) {
+for( i in 2:(length(fib_seq) - 1)) {
   fib_seq[i+1] <- fib_seq[i-1] + fib_seq[i]
 }
 fib_seq
 
+### Question 3
+## Redo question 4 from part 1 :
+
+## but this time store all the data. Make two vectors:
+## ("time" - stores the time steps (1-12) and "abundance" 
+## - stores the data on pop abundances predicted from the equation) 
+## then make a plot of the results.(time, abundances)
+
+Time <- rep(1, 12)
+Abundance <-  rep(2500,12)
+
+K <- 10000
+r <- 0.8
+
+for( i in seq(2,12) ) {
+  Time[i] <- i
+  Abundance[i] <- Abundance[i-1] + (r * (Abundance[i-1])) * ((K - (Abundance[i-1])) / K)
+  
+}
+Abundance
+
+plot(Time, Abundance)
+
+
+### Question 4: CO2!
+## make sure you have the .csv and the metadata files!
+## Read through the data!! (also make sure to set the directory)
+
+##4a. 
+setwd("~/Comp Bio/CompBioLabsAndHomework/Lab04")
+carbon <- read.csv("CO2_data_cut_paste.csv")
+##4b.
+### For each of the quantities, except for year, calculate 
+### the % change from year i-1 to year i across all years
+### Your results should be stored in a new data frame or matrix.
+### to do this efficiently, you may find that you need to use nested for loops
+
+head(carbon)
+
+## assign variables to vectors
+Total_carbon <- carbon$Total
+gas <- carbon$Gas
+liquids <- carbon$Liquids
+solids <- carbon$Solids
+cement <- carbon$CementProduction
+gas_flare <- carbon$gas_flare
+per_cap <- carbon$PerCapita
+
+### for total emissions, want a forloop to look like this
+delta_total <- rep(0, length(carbon$Year))
+for( i in 2:length(carbon$Year)){
+  delta_total[i] <- (Total_carbon[i] / Total_carbon[i-1]) * 100
+}
+
+### what if i do this for each column head...
+carbon_change <- rep(NA, length(carbon$Year))
+for (i in 2:length(colnames(carbon))) {
+  delta_carbon <- carbon[,i]
+  for (j in 2:length(delta_carbon)) {
+    carbon_change[j] <- (delta_carbon[j] - delta_carbon[j-1]) * 100
+  }
+}
+carbon_change
 
 
 
 
 
-
-
-
-
-
-
+##4c. use write.csv() to save your new data frame
+### to a file named "YearlyPercentChangesInCO2.csv"
 
 
 
